ba39fd9e3839b83535e569297d5d9e186c137f5e
albfernandes <deh.oo@hotmail.com>
2020-08-06 00:08:51 -0300

    feat: ensure mongodbaccountrepository returns an account on success

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,9 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  }
+  },
+  preset: '@shelf/jest-mongodb',
+  verbose: true
 }
